95e932d8ce9145d8b489f41d343010472c8d6a79
Arisa <15513600967@163.com>
2024-03-21 16:36:45 +0800

    功能：初始化DDD工程

---
+++ chatbot-api-interfaces/src/main/java/chatbot/api/ApiApplication.java
@@ -0,0 +1,14 @@
+package chatbot.api;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @description 启动入口
+ */
+
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class,args);
+    }
+
+}
---
+++ chatbot-api-interfaces/src/test/java/chatbot/api/test/ApiTest.java
@@ -0,0 +1,8 @@
+package chatbot.api.test;
+
+/**
+ * @description 单元测试
+ */
+
+public class ApiTest {
+}
